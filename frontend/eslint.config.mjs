baad9cb48775b043fadc28eb6fb45a791d2521ef
Tomy Hsieh <git@tomy.me>
2025-06-20 02:22:21 -0700

    🎛 ci: Add frontend CI

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,11 +1,14 @@
-import { FlatCompat } from "@eslint/eslintrc"
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
 ]
 
 export default eslintConfig
