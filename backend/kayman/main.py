018f1322cdf10f2fa4044b03ceb0665113ede141
Tomy Hsieh <git@tomy.me>
2025-09-01 16:50:36 -0700

    ❇️ refactor: Organize app init

--- backend/kayman/main.py
+++ backend/kayman/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from loguru import logger
@@ -9,13 +10,22 @@ from kayman.openapi import override_openapi
 from kayman.routers import routers, tags
 from kayman.util import KustomJSONResponse, custom_generate_unique_id
 
+cors_middleware = Middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app = FastAPI(
+    debug=settings.ENVIRONMENT == "local",
     title=settings.PROJECT_NAME,
     description="Kayman is the one-stop solution for personal finance",
     version="0.10.0",
-    debug=settings.ENVIRONMENT == "local",
-    default_response_class=KustomJSONResponse,
     openapi_tags=tags,
+    default_response_class=KustomJSONResponse,
+    middlewares=[cors_middleware],
     contact={"name": "Tomy Hsieh", "url": "https://github.com/tomy0000000"},
     license_info={
         "name": "MIT",
@@ -23,15 +33,7 @@ app = FastAPI(
     },
     generate_unique_id_function=custom_generate_unique_id,
 )
-logger.info(f"Applicaiton created in {settings.ENVIRONMENT} environment")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+logger.info(f"Application created in {settings.ENVIRONMENT} environment")
 
 # Auto migrate the database on startup
 if not app.debug:
