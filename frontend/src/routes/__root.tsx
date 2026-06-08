cefc6d689f42fefd7b24d7c46891465ac50704a0
Tomy Hsieh <git@tomy.me>
2026-06-08 01:45:37 -0700

    ✨ [feat][frontend] Production tenstack query devtool

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,9 +2,16 @@ import type { QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Suspense, lazy, useEffect, useState } from 'react'
 
 import { Toaster } from '@/components/ui/sonner'
 import type { useAuth } from '@/hooks/use-auth'
+
+declare global {
+  interface Window {
+    toggleDevtools: () => void
+  }
+}
 
 interface RouterContext {
   queryClient: QueryClient
@@ -15,13 +22,32 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootLayout
 })
 
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools
+    })
+  )
+)
+
 function RootLayout() {
+  const [showDevtools, setShowDevtools] = useState(false)
+
+  useEffect(() => {
+    window.toggleDevtools = () => setShowDevtools((old) => !old)
+  }, [])
+
   return (
     <>
       <Outlet />
       <Toaster />
       <TanStackRouterDevtools position="bottom-right" />
-      <ReactQueryDevtools buttonPosition="bottom-left" />
+      {showDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </Suspense>
+      )}
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right" />
     </>
   )
 }
