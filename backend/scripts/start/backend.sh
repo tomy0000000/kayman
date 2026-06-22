8a3fdfd91a4b2828526e63a51d8e447955caa5cb
Tomy Hsieh <git@tomy.me>
2026-06-22 00:48:31 -0700

    ⬆️ [bump][backend] `fastapi@0.138.0`

--- backend/scripts/start/backend.sh
+++ backend/scripts/start/backend.sh
@@ -3,11 +3,10 @@ set -euo pipefail
 #MISE description="Start backend server with hot reload"
 #MISE dir="backend"
 
-# TODO: when we upgrade to use FastAPI CLI, check
-# 1. if logging can be integrated with loguru
+# TODO: try to achieve these
+# 1. integrated fastapi CLI logging with loguru
 # 2. Mise task support colorful output for backend + frontend start task
-uv run uvicorn kayman.main:app \
+uv run fastapi dev kayman/main.py \
     --host 0.0.0.0 \
     --port 8000 \
-    --reload \
-    --log-level trace
+    --reload
