23f64828d565ea1ceffb32f09242aa3e5a91580d
Tomy Hsieh <git@tomy.me>
2026-06-28 03:41:01 -0700

    🕺🏻 [style][frontend] format UI

--- frontend/src/components/ui/kbd.tsx
+++ frontend/src/components/ui/kbd.tsx
@@ -1,6 +1,6 @@
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
-function Kbd({ className, ...props }: React.ComponentProps<"kbd">) {
+function Kbd({ className, ...props }: React.ComponentProps<'kbd'>) {
   return (
     <kbd
       data-slot="kbd"
@@ -13,11 +13,11 @@ function Kbd({ className, ...props }: React.ComponentProps<"kbd">) {
   )
 }
 
-function KbdGroup({ className, ...props }: React.ComponentProps<"div">) {
+function KbdGroup({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <kbd
       data-slot="kbd-group"
-      className={cn("inline-flex items-center gap-1", className)}
+      className={cn('inline-flex items-center gap-1', className)}
       {...props}
     />
   )
