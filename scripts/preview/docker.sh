ad083b960d4cde9e8d7467e0daf0b4e31bad082e
Tomy Hsieh <git@tomy.me>
2026-06-29 23:51:42 -0700

    🛠 [fix][tasks] Improve preview:docker task

--- scripts/preview/docker.sh
+++ scripts/preview/docker.sh
@@ -4,9 +4,13 @@ set -euo pipefail
 
 IMAGE_NAME="tomy0000000/kayman:latest"
 ENV_FILE="instance/development.env"
+CLIENTS_JSON="clients.json"
+HOST_PATH="./backend/instance/${CLIENTS_JSON}"
+MOUNT_PATH="/etc/secrets/${CLIENTS_JSON}"
 
 docker run \
     --rm \
     --env-file "${ENV_FILE}" \
-    -p 8000:8000 \
+    --volume "${HOST_PATH}:${MOUNT_PATH}" \
+    --publish 8000:8000 \
     "${IMAGE_NAME}"
