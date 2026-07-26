277954920eda76dd893b72f3904b2c4a1110f9a8
Tomy Hsieh <git@tomy.me>
2026-07-26 04:00:20 -0700

    ✨ [feat][backend] Update event entries API

--- backend/kayman/routers/event_entry.py
+++ backend/kayman/routers/event_entry.py
@@ -1,12 +1,18 @@
 from collections.abc import Sequence
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 
 from kayman.auth import get_client
 from kayman.core.db import get_session
 from kayman.crud.event_entry import create_event_entries
-from kayman.schemas.event_entry import EventEntryBase, EventEntryCreate, EventEntryRead
+from kayman.logics.event_entry import update_event_entries_by_ids
+from kayman.schemas.event_entry import (
+    EventEntryBase,
+    EventEntryCreate,
+    EventEntryRead,
+    EventEntryUpdate,
+)
 
 TAG_NAME = "Event Entry"
 tag = {
@@ -31,3 +37,17 @@ def create_entries(
     entries: list[EventEntryCreate],
 ) -> Sequence[EventEntryBase]:
     return create_event_entries(session, entries)
+
+
+@event_entry_router.patch(
+    "", name="Update Event Entries", response_model=list[EventEntryRead]
+)
+def update_entries(
+    *,
+    session: Session = Depends(get_session),
+    entries: list[EventEntryUpdate],
+) -> Sequence[EventEntryBase]:
+    try:
+        return update_event_entries_by_ids(session, entries)
+    except ValueError as err:
+        raise HTTPException(status_code=404, detail=err.args[0]) from err
