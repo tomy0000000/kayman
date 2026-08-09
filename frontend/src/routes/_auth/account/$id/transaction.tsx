3df7369bb482a3f4f784b93bafa03454c4962b96
Tomy Hsieh <git@tomy.me>
2026-08-08 23:50:40 -0700

    ❇️ [refactor][frontend] Move `transaction-fab.tsx`

--- frontend/src/routes/_auth/account/$id/transaction.tsx
+++ frontend/src/routes/_auth/account/$id/transaction.tsx
@@ -6,8 +6,8 @@ import { toast } from 'sonner'
 
 import { DatePickerWithRange } from '@/components/date-range-picker'
 import { CreateEventSheet } from '@/components/event/create-event-sheet'
-import { TransactionFab } from '@/components/transaction-fab'
 import { TransactionPostSheet } from '@/components/transaction-post-sheet'
+import { TransactionFab } from '@/components/transaction/transaction-fab'
 import { TransactionsTable } from '@/components/transaction/transactions-table'
 import { Separator } from '@/components/ui/separator'
 import {
